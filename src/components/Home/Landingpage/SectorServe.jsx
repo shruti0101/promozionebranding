93e9bb5e6f9ea27d833a2a32dccc586b8d4605bb
shruti0101 <shrutiguptacu@gmail.com>
2025-08-07 15:09:39 +0530

    commit final

--- src/components/Home/Landingpage/SectorServe.jsx
+++ src/components/Home/Landingpage/SectorServe.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import "../../../styles/Sectorserve.css";
+import "../../../styles/SectorServe.css";
 
 // Images
 import packaging from "../../../assets/package.webp";
