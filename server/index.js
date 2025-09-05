e56624810bb40f32423e3abe638299e41edb09ec
shruti0101 <shrutiguptacu@gmail.com>
2025-09-05 17:50:11 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -16,4 +16,4 @@ app.use("/api/blog",require ("./routes/blogRoute"))
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,"0.0.0.0" ,() => console.log(`Server running on port ${PORT}`));
