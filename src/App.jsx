7fe79fd4b78dba30782551ad444b7bf9b7003d50
shruti0101 <shrutiguptacu@gmail.com>
2025-11-10 14:58:15 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,8 @@ import Socialpackage from "./pages/Socialpackage/Socialpackage";
 import Adspackage from "./pages/Adspackage/Adspackage";
 import Whatsapp from "./components/Whatsapp";
 import BlogPost from "./pages/Blogs/BlogPost";
-import Citypage from "./pages/Citypage/Citypage";
+import CityPage from "./pages/CityPage/Citypage";
+
 
 import { serviceLocations } from "./citydata/Citydata";
 
@@ -94,7 +95,7 @@ const App = () => {
         {serviceLocations.map(({ id, link }) => {
           // Remove leading slash if present
           const path = link.startsWith("/") ? link.slice(1) : link;
-          return <Route key={id} path={path} element={<Citypage />} />;
+          return <Route key={id} path={path} element={<CityPage />} />;
         })}
 
 
